40004657b60a1527856a0d5ecadf804bc9db7404
jonas.americo <jjsamerico@gmail.com>
2023-08-02 06:20:14 -0300

    Basico de objetos de configuracao
    variaveis de ambiente
    injetando configmap
    secrets

--- full-cycle-kubernetes/server.go
+++ full-cycle-kubernetes/server.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("oi")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/secret", secret)
+	http.HandleFunc("/configmap", configMap)
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
@@ -17,5 +21,23 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
+}
+
+func configMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
